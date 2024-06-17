1ae26d0d3a87d0d84b06377403eb0238e85688e0
Garland Xie <garlandxie@gmail.com>
2024-06-17 15:36:00 -0400

    Calculate germination speed

--- scripts/processing_scripts/01.data_cleaning.R
+++ scripts/processing_scripts/01.data_cleaning.R
@@ -6,6 +6,7 @@ library(tidyr)      # for handling missing values
 library(tidyr)      # for replacing missing values
 library(lubridate)  # for dealing with dates
 library(stringr)    # for dealing with string characters
+library(GerminaR)   # for summarizing germination indices
 
 # import -----------------------------------------------------------------------
 
@@ -239,9 +240,203 @@ c_germ_perc <- c_germ_perc_ciar %>%
 # at the end of the experiment 
 c_germ_w12 <- dplyr::filter(c_germ_perc, week == 12) 
 
+## germination speed ------------------------------------------------------------
+
+# calculate the number of seeds germinated per week for each samples species
+c_gs_w1 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 1 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W1" = "1")
+
+c_gs_w2 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 2 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W2" = "2")
+
+c_gs_w3 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 3 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W3" = "3")
+
+c_gs_w4 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 4 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ)  %>%
+  ungroup() %>%
+  rename("W4" = "4")
+
+c_gs_w5 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 5 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W5" = "5")
+
+c_gs_w6 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 6 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ)  %>%
+  ungroup() %>%
+  rename("W6" = "6")
+
+c_gs_w7 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 7 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W7" = "7")
+
+c_gs_w8 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 8 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W8" = "8")
+  
+c_gs_w9 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 9 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W9" = "9")
+
+c_gs_w10 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 10 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W10" = "10")
+
+c_gs_w11 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 11 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W11" = "11")
+
+c_gs_w12 <- c_germ_raw %>%
+  dplyr::select(week, richness_id, density_id, rep, spp, sown_seeds, cum_germ) %>%
+  filter(week == 12 & richness_id != "-") %>%
+  group_by(richness_id, density_id, rep, spp) %>%
+  tidyr::pivot_wider(names_from = week, values_from = cum_germ) %>%
+  ungroup() %>%
+  rename("W12" = "12")
+
+multi_key_id <- c("richness_id", "density_id", "rep", "spp", "sown_seeds")
+
+c_gs <- c_gs_w1 %>%
+  inner_join(c_gs_w2, by = multi_key_id) %>%
+  inner_join(c_gs_w3, by = multi_key_id) %>%
+  inner_join(c_gs_w4, by = multi_key_id) %>%
+  inner_join(c_gs_w5, by = multi_key_id) %>%
+  inner_join(c_gs_w6, by = multi_key_id) %>%
+  inner_join(c_gs_w7, by = multi_key_id) %>%
+  inner_join(c_gs_w8, by = multi_key_id) %>%
+  inner_join(c_gs_w9, by = multi_key_id) %>%
+  inner_join(c_gs_w10, by = multi_key_id) %>%
+  inner_join(c_gs_w11, by = multi_key_id) %>%
+  inner_join(c_gs_w12, by = multi_key_id) 
+
+c_gs2 <- c_gs %>%
+  mutate(
+    W2_diff = W2 - W1, 
+    W3_diff = W3 - W2, 
+    W4_diff = W4 - W3, 
+    W5_diff = W5 - W4, 
+    W6_diff = W6 - W5, 
+    W7_diff = W7 - W6, 
+    W8_diff = W8 - W7, 
+    W9_diff = W9 -W8, 
+    W10_diff = W10 - W9, 
+    W11_diff = W11 - W10, 
+    W12_diff = W12 - W11
+    ) %>%
+  select(
+    richness_id, 
+    density_id, 
+    rep, 
+    spp, 
+    sown_seeds, 
+    W1, 
+    W2_diff, 
+    W3_diff, 
+    W4_diff, 
+    W5_diff, 
+    W6_diff, 
+    W7_diff, 
+    W8_diff, 
+    W9_diff, 
+    W10_diff, 
+    W11_diff, 
+    W12_diff
+  ) 
+
+# calculate germination speed per sampled species
+c_gs2 <- c_gs2 %>%
+    mutate(
+    mgt = ger_MGT(evalName = "W", data = .),
+    gsp = ger_GSP(evalName = "W", data = .)   
+    ) 
+
+# calculate germination speed and mean germination time per tray
+# for the resident community 
+c_gs_res <- c_gs2 %>%
+  dplyr::filter(!(spp %in% c("CIAR"))) %>%
+  group_by(richness_id, density_id, rep) %>%
+  summarize(
+    mean_gsp = mean(gsp, na.rm = TRUE),
+    mean_mgt = mean(mgt, na.rm = TRUE)
+  ) %>%
+  ungroup() 
+
+# create a dataset with germination speed, mean germination time, 
+# and percent germination for each tray
+c_germ <- left_join(
+  c_germ_w12, 
+  c_gs_res, 
+  by = c("richness_id", "density_id", "rep")
+)
+
+# some exploratory analyses ----------------------------------------------------
+
+# may move this to another R script
+c_germ %>%
+  ggplot(aes(x = density_id, y = mean_gsp)) + 
+  geom_boxplot()
+
+summary(lm(mean_gsp ~ density_id*richness_id, 
+            data = c_germ))
+
+c_germ %>%
+  ggplot(aes(x = mean_gsp, y = cum_germ_perc_res)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se = FALSE)
+
+summary(glm(cum_germ_perc_res ~ mean_gsp, 
+            weights = sown_seeds,
+            family = binomial(link = "logit"), 
+            data = c_germ))
+
 # save to disk -----------------------------------------------------------------
 
-## invader biomass -------------------------------------------------------------
+## invader biomass -----------------------s--------------------------------------
 
 write.csv(
   biomass_ab_invasive, 
@@ -277,5 +472,5 @@ write.csv(
 
 write.csv(
   c_germ_w12, 
-  file = here("data", "intermediate_data", "c_germ_week12.csv")
+  file = here("data", "intermediate_data", "c_germ_week.csv")
 )
